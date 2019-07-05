b7e25f5e5b607b2ef853772ebbf7b3cafcb7b493
eu/githubDesktop <github-desktop@dejandev.eu>
2019-07-05 18:57:35 +0200

    renamed file

--- src/SparkFunMPU9250-DMP-EPS.cpp
+++ src/SparkFunMPU9250-DMP-EPS.cpp
@@ -15,7 +15,7 @@ SparkFun 9DoF Razor IMU M0
 Supported Platforms:
 - ATSAMD21 (Arduino Zero, SparkFun SAMD21 Breakouts)
 ******************************************************************************/
-#include "SparkFunMPU9250-DMP.h"
+#include "SparkFunMPU9250-DMP-ESP.h"
 #include "MPU9250_RegisterMap.h"
 
 extern "C" {
